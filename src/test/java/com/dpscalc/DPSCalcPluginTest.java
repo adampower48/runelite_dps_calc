5fcba5a6c3c75356be303e5a4646949bc7f20a54
adampower48 <adam.power48@mail.dcu.ie>
2022-03-30 20:21:25 +0100

    Update example fields in template.

--- src/test/java/com/dpscalc/DPSCalcPluginTest.java
+++ src/test/java/com/dpscalc/DPSCalcPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.dpscalc;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class DPSCalcPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(DPSCalcPlugin.class);
 		RuneLite.main(args);
 	}
 }
